0d715d634e7d0328acf315e7d994d750555a89e9
travistrue2008 <travis.true08@gmail.com>
2025-02-27 22:07:14 -0800

    Updated

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,4 +1,11 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.89.0"
+    }
+  }
+
   backend "s3" {
     use_lockfile = true
     region       = "us-east-1"
